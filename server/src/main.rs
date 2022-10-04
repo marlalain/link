442e6be5d3eeca9f00a68fe5ac2995843d628e0b
Alex <@.gg>
2022-10-03 22:35:14 -0300

    maybe

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,13 +6,20 @@ use axum::body::{Bytes, Empty};
 use axum::http::StatusCode;
 use axum::response::Response;
 use axum::routing::get;
+use axum::extract::{Path};
 
 async fn redirect() -> Response<Empty<Bytes>> {
-    println!("redirecting...");
-
     Response::builder()
       .status(StatusCode::PERMANENT_REDIRECT)
-      .header("Location", "https://cdn.discordapp.com/attachments/885709126267732029/1026639328140599316/8db5a3c3dbf13c701c46b7b28a3c685d.jpg")
+      .header("location", "https://cdn.discordapp.com/attachments/885709126267732029/1026639328140599316/8db5a3c3dbf13c701c46b7b28a3c685d.jpg")
+      .body(Empty::new())
+      .unwrap()
+}
+
+async fn discord(Path(id): Path<String>) -> Response<Empty<Bytes>> {
+    Response::builder()
+      .status(StatusCode::PERMANENT_REDIRECT)
+      .header("location", format!("https://cdn.discordapp.com/attachments/{}", id))
       .body(Empty::new())
       .unwrap()
 }
@@ -20,6 +27,7 @@ async fn redirect() -> Response<Empty<Bytes>> {
 #[tokio::main]
 async fn main() {
     let app = Router::new()
+      .route("/d/:id", get(discord))
       .route("/profile", get(redirect));
     let ip = env::var("IP").unwrap_or("0.0.0.0".parse().unwrap());
     let port = env::var("PORT").unwrap_or("80".parse().unwrap());
