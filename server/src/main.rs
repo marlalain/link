126a321b6126e9dc4c2c22f8b653f34806403e9c
Alex <@.gg>
2022-10-03 22:50:06 -0300

    feat: github redirect

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,7 +8,15 @@ use axum::response::Response;
 use axum::routing::get;
 use axum::extract::{Path};
 
-async fn redirect() -> Response<Empty<Bytes>> {
+async fn github() -> Response<Empty<Bytes>> {
+    Response::builder()
+      .status(StatusCode::PERMANENT_REDIRECT)
+      .header("location", "https://github.com/minsk-dev")
+      .body(Empty::new())
+      .unwrap()
+}
+
+async fn profile() -> Response<Empty<Bytes>> {
     Response::builder()
       .status(StatusCode::PERMANENT_REDIRECT)
       .header("location", "https://cdn.discordapp.com/attachments/885709126267732029/1026639328140599316/8db5a3c3dbf13c701c46b7b28a3c685d.jpg")
@@ -17,8 +25,6 @@ async fn redirect() -> Response<Empty<Bytes>> {
 }
 
 async fn discord(Path((id, id2, img)): Path<(String, String, String)>) -> Response<Empty<Bytes>> {
-    println!("https://cdn.discordapp.com/attachments/{}/{}/{}", id, id2, img);
-
     Response::builder()
       .status(StatusCode::PERMANENT_REDIRECT)
       .header("location", format!("https://cdn.discordapp.com/attachments/{}/{}/{}", id, id2, img))
@@ -30,7 +36,7 @@ async fn discord(Path((id, id2, img)): Path<(String, String, String)>) -> Respon
 async fn main() {
     let app = Router::new()
       .route("/d/:id/:id2/:img", get(discord))
-      .route("/profile", get(redirect));
+      .route("/profile", get(profile));
     let ip = env::var("IP").unwrap_or("0.0.0.0".parse().unwrap());
     let port = env::var("PORT").unwrap_or("80".parse().unwrap());
     let addr = format!("{}:{}", ip, port);
